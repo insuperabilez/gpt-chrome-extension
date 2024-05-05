dc70b3f885edfaf9d815e281c6458a7eed5cbfd7
insuperabilez <insuperabilehart@gmail.com>
2024-05-05 11:39:39 +0400

    add-image

--- popup.js
+++ popup.js
@@ -1,7 +1,7 @@
 document.getElementById('sendRequest').addEventListener('click', function() {
   var text = document.getElementById('inputText').value;
   var url = 'https://gpt-server-thyx.onrender.com/gpt?text=' + encodeURIComponent(text);
-  document.getElementById('response').textContent = "Обработка запроса...";
+  document.getElementById('response').textContent = "Preparing answer...";
   fetch(url)
     .then(response => response.text())
     .then(data => {
